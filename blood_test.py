9b43e20b5810309f6daedace48d7e98375ab3f0e
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2023-05-02 18:26:56 +0200

    Display before/after desire date

--- blood_test.py
+++ blood_test.py
@@ -1,5 +1,7 @@
+import datetime
 import tkinter as tk
 from tkinter import ttk
+import pandas as pd
 
 import openpyxl
 import os
@@ -38,10 +40,61 @@ def load_patient_data():
     date_picker['values'] = dates
 
 
-def display_data():
+def display_data_by_selected_date():
     date = date_var.get()
     result.delete('1.0', tk.END)
     result.insert(tk.END, '\n'.join(date_data[date]))
+
+
+def display_data_before():
+    result.delete('1.0', tk.END)
+
+    desired_date_str = date_entry.get().strip()
+    if not desired_date_str:
+        return
+
+    desired_date = datetime.datetime.strptime(desired_date_str, '%d-%m-%y')
+
+    data_before = []
+
+    for date_str in date_data.keys():
+        date = datetime.datetime.strptime(date_str, '%d-%m-%y %H:%M')
+        if date <= desired_date:
+            data_before.append(date_str)
+            data_before.extend(date_data[date_str])
+            data_before.append('\n\n')  # add an empty line between each set of data
+
+    result_text = '\n'.join(data_before)
+    result.insert(tk.END, result_text)
+
+
+def display_data_after():
+    result.delete('1.0', tk.END)
+
+    desired_date_str = date_entry.get().strip()
+    if not desired_date_str:
+        return
+
+    desired_date = datetime.datetime.strptime(desired_date_str, '%d-%m-%y')
+
+    data_after = []
+
+    for date_str in date_data.keys():
+        date = datetime.datetime.strptime(date_str, '%d-%m-%y %H:%M')
+        if date >= desired_date:
+            data_after.append(date_str)
+            data_after.extend(date_data[date_str])
+            data_after.append('\n\n')  # add an empty line between each set of data
+
+    result_text = '\n'.join(data_after)
+    result.insert(tk.END, result_text)
+
+
+def display_data():
+    if radio_var.get() == "before":
+        display_data_before()
+    else:
+        display_data_after()
 
 
 root = tk.Tk()
@@ -68,8 +121,26 @@ date_label.pack()
 date_picker = ttk.Combobox(root, textvariable=date_var)
 date_picker.pack()
 
-display_button = tk.Button(root, text="Display Data", command=display_data)
+display_button = tk.Button(root, text="Display Data", command=display_data_by_selected_date)
 display_button.pack()
+
+date_prompt_label = tk.Label(root, text="Enter a desired date (MM-DD-YY):")
+date_prompt_label.pack()
+
+date_entry = tk.Entry(root)
+date_entry.pack()
+
+radio_var = tk.StringVar()
+radio_var.set("before")
+
+radio_before = tk.Radiobutton(root, text="Before", variable=radio_var, value="before")
+radio_before.pack()
+
+radio_after = tk.Radiobutton(root, text="After", variable=radio_var, value="after")
+radio_after.pack()
+
+submit_button = tk.Button(root, text="Submit", command=display_data)
+submit_button.pack()
 
 result = tk.Text(root, wrap=tk.WORD, width=80, height=30)
 result.pack()
